ba13aa7179cb0cf5c7979e47ce401a7e1b08e458
TheBarbellDemon <thebarbelldemon@gmail.com>
2017-11-22 19:30:55 +0000

    updating SortFile with a test file

--- SortFile.groovy
+++ SortFile.groovy
@@ -1,2 +1,11 @@
 #!/usr/bin/groovy
-println "${args[0]}"
+def list = new File(args[0]).collect{it}
+
+Collections.sort list
+
+new File(args[1]).withWriter('utf-8'){
+	writer ->
+		list.each{
+			writer.println it
+		}
+}
